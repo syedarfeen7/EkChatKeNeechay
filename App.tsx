0d16e994f7cda801be16550e2c02b7e1dbe61e5b
syedarfeen7 <syedarfeen7890@gmail.com>
2025-01-01 15:49:07 +0300

    chore: yarn pod setup

--- App.tsx
+++ App.tsx
@@ -71,25 +71,12 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            alignItems: "center",
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="Autohub">Hello World!</Section>
         </View>
       </ScrollView>
     </SafeAreaView>
