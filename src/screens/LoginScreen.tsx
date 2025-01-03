c926d4ede40f1e16f87aff82e473fc8716104ead
syedarfeen7 <syedarfeen7890@gmail.com>
2025-01-03 20:18:16 +0300

    feat: Login screen (In progress)
    font awesome icons, validations and translations added

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   TouchableOpacity,
-  Button,
+  Image,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import {useForm, Controller} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../types/navigation';
 import {loginSchema} from '../utils/validations';
 import {useTranslation} from 'react-i18next';
+import LanguageToggle from '../components/LanguageToggle';
+import images from '../asstes';
+import {currentLanguage} from '../helpers/common';
 
 type LoginScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -24,12 +28,12 @@ type Props = {
 };
 
 interface FormValues {
-  email: string;
-  password: string;
+  phone: string;
 }
 
 const LoginScreen: React.FC<Props> = ({navigation}) => {
-  const {t, i18n} = useTranslation();
+  const {t} = useTranslation();
+  const [isFocused, setIsFocused] = useState(false);
 
   const {
     control,
@@ -40,73 +44,92 @@ const LoginScreen: React.FC<Props> = ({navigation}) => {
   });
 
   const onSubmit = (data: FormValues) => {
-    // Replace this with actual authentication logic
     console.log('Login data:', data);
     navigation.navigate('Home');
   };
 
   return (
-    <View style={styles.container}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>{t('login')}</Text>
-        <Button
-          title="Change to Arabic"
-          onPress={() => i18n.changeLanguage('ar')}
+    <View style={styles.mainContainer}>
+      <LanguageToggle />
+      <View style={styles.container}>
+        <View style={styles.imageWrapper}>
+          <Image
+            source={currentLanguage() === 'en' ? images.enLogo : images?.arLogo}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+          <Text style={styles.title}>{t('login')}</Text>
+        </View>
+
+        <Controller
+          name="phone"
+          control={control}
+          render={({field: {onChange, onBlur, value}}) => (
+            <View
+              style={[
+                styles.phoneInputContainer,
+                errors.phone && styles.errorInput,
+                isFocused && styles.isFocusedStyle,
+              ]}>
+              <Icon name="phone" size={20} style={styles.phoneIcon} />
+              <Text style={styles.countryCode}>+966</Text>
+              <TextInput
+                style={styles.phoneInput}
+                placeholder="Enter phone number"
+                keyboardType="phone-pad"
+                onChangeText={onChange}
+                value={value}
+                maxLength={9}
+                onBlur={() => {
+                  onBlur();
+                  setIsFocused(false); // On blur, remove focus
+                }}
+                onFocus={() => setIsFocused(true)}
+              />
+            </View>
+          )}
         />
-        <Button
-          title="Change to English"
-          onPress={() => i18n.changeLanguage('en')}
-        />
+        {errors.phone && (
+          <Text style={styles.errorText}>{errors.phone.message}</Text>
+        )}
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit(onSubmit)}>
+          <Text style={styles.buttonText}>{t('login')}</Text>
+        </TouchableOpacity>
       </View>
-      <Controller
-        name="email"
-        control={control}
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            style={[styles.input, errors.email && styles.errorInput]}
-            placeholder="Email"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
-      />
-      {errors.email && (
-        <Text style={styles.errorText}>{errors.email.message}</Text>
-      )}
-
-      <Controller
-        name="password"
-        control={control}
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            style={[styles.input, errors.password && styles.errorInput]}
-            placeholder="Password"
-            secureTextEntry
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
-      />
-      {errors.password && (
-        <Text style={styles.errorText}>{errors.password.message}</Text>
-      )}
-
-      <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', padding: 20},
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  imageWrapper: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 28,
+    textTransform: 'uppercase',
     marginBottom: 20,
+    width: '100%',
     textAlign: 'center',
+    color: '#000',
+  },
+  logo: {
+    width: 220,
+    marginBottom: 20,
   },
   input: {
     borderWidth: 1,
@@ -115,10 +138,53 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
   },
-  errorInput: {borderColor: 'red'},
-  errorText: {color: 'red', marginBottom: 10},
-  button: {backgroundColor: '#007BFF', padding: 15, borderRadius: 5},
-  buttonText: {color: 'white', textAlign: 'center', fontWeight: 'bold'},
+  phoneInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 30,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  phoneIcon: {
+    marginRight: 10,
+    marginLeft: 10,
+    color: '#05c3de',
+  },
+  countryCode: {
+    fontSize: 16,
+    color: '#808080',
+    marginRight: 10,
+    fontWeight: '700',
+  },
+  phoneInput: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 15,
+  },
+  errorInput: {
+    borderColor: 'red',
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#ffb71b',
+    padding: 15,
+    borderRadius: 30,
+    marginTop: 20,
+    width: 250,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  isFocusedStyle: {
+    borderColor: '#05c3de',
+  },
 });
 
 export default LoginScreen;
--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,8 +1,8 @@
+import i18n from '../i18n';
 import * as Yup from 'yup';
 
 export const loginSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string()
-    .min(6, 'Password must be at least 6 characters')
-    .required('Password is required'),
+  phone: Yup.string()
+    .required(i18n.t('validations.phoneNumber'))
+    .matches(/^[0-9]{9}$/, i18n.t('validations.phoneNumberInvalid')),
 });
