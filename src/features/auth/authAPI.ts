f7fe14cff3bce51474fdf4fdebc0b08942a0be47
syedarfeen7 <syedarfeen7890@gmail.com>
2025-01-05 17:58:32 +0300

    chore: update login api and integrated loader actions

--- src/features/auth/authAPI.ts
+++ src/features/auth/authAPI.ts
@@ -1,14 +1,25 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {LoginPayload, User} from './authTypes';
+import {showLoader, hideLoader} from '../loader/loaderSlice';
 
 export const loginUserAPI = createAsyncThunk(
   'auth/login',
-  async (payload: LoginPayload): Promise<User> => {
-    const {phone} = payload;
-    if (phone) {
-      return {id: 1, name: 'Test User', phone};
-    } else {
-      throw new Error('Invalid user');
+  async (payload: LoginPayload, {dispatch}): Promise<User> => {
+    try {
+      dispatch(showLoader());
+
+      const {phone} = payload;
+      if (phone) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        return {id: 1, name: 'Test User', phone};
+      } else {
+        throw new Error('Invalid user');
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      dispatch(hideLoader());
     }
   },
 );
