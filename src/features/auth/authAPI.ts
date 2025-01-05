d492cb27d96bb633389368cf579d100a9f762bff
syedarfeen7 <syedarfeen7890@gmail.com>
2025-01-05 23:07:21 +0300

    chore: register API handler

--- src/features/auth/authAPI.ts
+++ src/features/auth/authAPI.ts
@@ -1,20 +1,48 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
-import {LoginPayload, User} from './authTypes';
-import {showLoader, hideLoader} from '../loader/loaderSlice';
+import {hideLoader, showLoader} from '../loader/loaderSlice';
+import {LoginOtp, LoginPayload, RegisterPayload, User} from './authTypes';
 
 export const loginUserAPI = createAsyncThunk(
   'auth/login',
-  async (payload: LoginPayload, {dispatch}): Promise<User> => {
+  async (
+    {payload, navigation}: {payload: LoginPayload; navigation: any},
+    {dispatch},
+  ): Promise<LoginOtp> => {
     try {
       dispatch(showLoader());
 
       const {phone} = payload;
       if (phone) {
         await new Promise(resolve => setTimeout(resolve, 1000));
-
-        return {id: 1, name: 'Test User', phone};
+        navigation.navigate('OtpScreen');
+        return {otp: '12345'};
       } else {
         throw new Error('Invalid user');
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      dispatch(hideLoader());
+    }
+  },
+);
+
+export const registerUserAPI = createAsyncThunk(
+  'auth/register',
+  async (
+    {payload, navigation}: {payload: RegisterPayload; navigation: any},
+    {dispatch},
+  ): Promise<User> => {
+    try {
+      dispatch(showLoader());
+
+      const {firstName, lastName, email, phone, termsAccepted} = payload;
+      if (firstName && lastName && phone && email && termsAccepted) {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        navigation.navigate('Login');
+        return {firstName, lastName, email, phone, termsAccepted};
+      } else {
+        throw new Error('Registration Failed!');
       }
     } catch (error) {
       throw error;
