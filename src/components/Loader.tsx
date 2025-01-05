52f8dd126c9f85c417ecbd22bfe0ee4c84592db0
syedarfeen7 <syedarfeen7890@gmail.com>
2025-01-05 17:37:40 +0300

    chore: created loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, ActivityIndicator, StyleSheet, Modal} from 'react-native';
+
+interface LoaderProps {
+  visible: boolean;
+}
+
+const Loader: React.FC<LoaderProps> = ({visible}) => {
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <Modal transparent animationType="fade" visible={visible}>
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#ffb71b" />
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+});
+
+export default Loader;
