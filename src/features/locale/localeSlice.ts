6f4f7849e5bf3e293d9f55b80b81a5099efeae18
syedarfeen7 <syedarfeen7890@gmail.com>
2025-04-17 17:29:29 +0300

    chore: translations work

---
+++ src/features/locale/localeSlice.ts
@@ -0,0 +1,25 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+export type Language = 'en' | 'ar' | 'ur';
+
+interface LanguageState {
+  selectedLanguage: Language;
+}
+
+const initialState: LanguageState = {
+  selectedLanguage: 'ar',
+};
+
+const languageSlice = createSlice({
+  name: 'language',
+  initialState,
+  reducers: {
+    changeLanguage(state, action: PayloadAction<Language>) {
+      state.selectedLanguage = action.payload;
+    },
+  },
+});
+
+export const {changeLanguage} = languageSlice.actions;
+
+export default languageSlice.reducer;
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,11 +2,13 @@ import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import en from './locales/en/translation.json';
 import ur from './locales/ur/translation.json';
+import ar from './locales/ar/translation.json';
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {translation: en},
     ur: {translation: ur},
+    ar: {translation: ar},
   },
   lng: 'en', // Default language
   fallbackLng: 'en',
@@ -14,5 +16,17 @@ i18n.use(initReactI18next).init({
     escapeValue: false, // React already escapes strings
   },
 });
+
+export const i18nChangeLanguage = (language: string) => {
+  i18n.changeLanguage(language);
+};
+
+export function isRTL(): boolean {
+  return i18n.language === 'ar' || i18n.language === 'ur';
+}
+
+export function currentLang(): string {
+  return i18n.language;
+}
 
 export default i18n;
